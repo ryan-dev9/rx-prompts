7c45b1558b979acaa15b18c5cff76434b6cb32ff
ryan-dev9 <ryandeveloper97@gmail.com>
2025-08-23 17:00:18 +0530

    Improve-S2.0

--- app/layout.js
+++ app/layout.js
@@ -25,6 +25,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={[orbitron.className, geistSans.className]}>
       <body>
+        <meta name="google-site-verification" content="Se1a7QWwwyRyhtf7KFVk8WEjsyRJn3BxcHMvnGFs2TE" />
         <Navbar />
         {children}
         <Analytics />
