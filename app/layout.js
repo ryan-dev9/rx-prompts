d4f9b6206d53b910de700fb750c11fdc57f05536
ryan-dev9 <ryandeveloper97@gmail.com>
2025-08-26 16:41:52 +0530

    IS2.2

--- app/layout.js
+++ app/layout.js
@@ -43,7 +43,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={[orbitron.className, geistSans.className]}>
       <body>
+        <head>
         <meta name="google-site-verification" content="Se1a7QWwwyRyhtf7KFVk8WEjsyRJn3BxcHMvnGFs2TE" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8914479693209883"
+        crossorigin="anonymous"></script>
+        </head>
         <Navbar />
         {children}
         <Analytics />
